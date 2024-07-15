9a33fd8808fc2f3867ba0fbe959e3d4eff47a427
Fihliwe <136016375+Fihliwe@users.noreply.github.com>
2024-07-15 11:00:29 +0000

    feat: setting state values for invoice

--- invoease/src/App.js
+++ invoease/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Footer from "./App components/footer";
 import Notes from "./App components/notes";
 import Header from "./App components/header";
@@ -7,17 +8,23 @@ import Date from "./App components/Date";
 import Table from "./App components/table";
 
 function App() {
+  // state values to toggle between the form and invoice
+  const [showInvoice, setShowInvoice] = useState(false);
 
   return (
     <>
       <main className="m-5 p-5 xl:m-w-4xl xl:mx-auto bg-white rounded shadow">
-        <Header />
-        <Details />
-        <ClientsDetails />
-        <Date />
-        <Table />
-        <Notes />
-        <Footer />
+        {showInvoice ? <div>
+          <Header />
+          <Details />
+          <ClientsDetails />
+          <Date />
+          <Table />
+          <Notes />
+          <Footer />
+        </div> : (
+          <input type="text" name="text" id="text" placeholder="Enter your name" />
+        )}
       </main>
     </>
   );
